960ef7f499d8671260283a5b5fd0f29d3a71f9f7
Ryu-02 <ymq02110192@gmail.com>
2024-12-26 16:57:30 +0900

    nodemon 적용

--- app/app.js
+++ app/app.js
@@ -1,13 +1,17 @@
 "use strict";
 
-const PORT = 3000;
-
+// 모듈
 const express = require("express");
-
 const app = express();
 
-app.get("/", function (req, res) {
-  res.send("Hello World");
-});
+//앱 라우팅
+const home = require("./src/routes/home");
 
-app.listen(PORT, () => console.log("server start"));
+//앱 세팅
+app.set("views", "./src/views");
+app.set("view engine", "ejs");
+app.use(express.static(`${__dirname}/src/public`));
+
+app.use("/", home); // use -> 미들 웨어를 등록해주는 메서드.
+
+module.exports = app;
