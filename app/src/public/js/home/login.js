2c1a029ba609e6b452f5d374e0af9ad8064f83ef
Ryu-02 <ymq02110192@gmail.com>
2025-01-03 14:43:03 +0900

    로그인 프론트엔드 구현

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -13,13 +13,23 @@ function login(event) {
     id: id.value,
     pwd: pwd.value,
   };
-  console.log(req);
 
-  loginAdmin(req);
-}
-
-function loginAdmin(req) {
-  if (req.id === "admin") {
-    console.log("로그인 성공");
-  }
+  fetch("/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(req),
+  })
+    .then((res) => res.json())
+    .then((res) => {
+      if (res.success) {
+        location.href = "/";
+      } else {
+        alert(res.msg);
+      }
+    })
+    .catch((err) => {
+      console.error(new Error("로그인 중 에러 발생"));
+    });
 }
